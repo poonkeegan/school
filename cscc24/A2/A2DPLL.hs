c8b716c757e4b52d9efeccdcf9bac91c7dcbc16c
poonkeegan <poonkeegan@gmail.com>
2018-02-04 20:28:24 -0500

    Added D37

--- cscc24/A2/A2DPLL.hs
+++ cscc24/A2/A2DPLL.hs
@@ -6,6 +6,32 @@ module A2DPLL where
 
 data Atom = Pos String | Neg String deriving Show
 
+atomEq :: Atom -> Atom -> Bool
+atomEq (Pos str1) (Pos str2) = str1 == str2
+atomEq (Neg str1) (Neg str2) = str1 == str2
+atomEq _ _ = False
+
+atomNeq :: Atom -> Atom -> Bool
+atomNeq x y = not (atomEq x y)
+
+atomNeg :: Atom -> Atom
+atomNeg (Pos str) = Neg str
+atomNeg (Neg str) = Pos str
+
 -- True if satisfiable, False if unsatisfiable.
 cnfSAT :: [[Atom]] -> Bool
-cnfSAT = error "TODO"
+cnfSAT [] = True
+cnfSAT ([]:_) = False
+cnfSAT lst((x:_):_) = or (map cnfSAT [filtercnf lst x, filtercnf lst (atomNeg x)])
+
+atomInLst :: Atom -> [Atom] -> Bool
+atomInLst element lst = or (map (atomEq element) lst)
+
+filtercnf lst x = map (filter (atomNeq nx)) reduced
+    where reduced = filter (\y -> not (atomInLst x y)) lst
+          nx = atomNeg x
+
+examples = [[Pos "b", Pos "c"],
+            [Pos "a", Neg "b", Neg "d"],
+            [Neg "a", Pos "b", Neg "c", Pos "d"]]
+
