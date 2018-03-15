146a1fcfae18f3a4c60ea7c4c7b518adba47314e
Mustafa Quraish <mustafaq9@gmail.com>
2018-03-15 14:15:41 -0400

    more a4 work

--- cscc24/A4/A4TermParser.hs
+++ cscc24/A4/A4TermParser.hs
@@ -34,33 +34,52 @@ boolPars = trueParse <|> falseParse
     where trueParse = litPars (Bln True) "True"
           falseParse = litPars (Bln False) "False"
 
+boolOpPars = (andParse <|> orParse) <* whitespaces
+    where andParse = string "&&"
+          orParse = string "||"
+
 cmpPars = (eqstr <|> neqstr <|> ltstr <|> leqstr) <* whitespaces
     where eqstr = string "=="
           neqstr = string "/="
           ltstr = string "<"
           leqstr = string "<="
 
-facPars = hd
+facPars = parsFoldl App hd tl
     where ls = some atomPars
           hd = fmap head ls
           tl = fmap tail ls
+
+addendPars = parsFoldl binop hd tl
+    where facOpPars = (\x y -> Prim2 y x) <$> facPars <*> mulopPars
+          factors = many facOpPars
+          hd = fmap head factors
+          tl = fmap tail factors
+          binop a b = (\x -> a (b x))
+
+infix = parsFoldl binop hd tl
+    where facOpPars = (\x y -> Prim2 y x) <$> facPars <*> mulopPars
+          factors = many facOpPars
+          hd = fmap head factors
+          tl = fmap tail factors
+          binop a b = (\x -> a (b x))
+parsFoldl op parseHd parseTl = ((foldl op) <$> parseHd) <*> parseTl
 
 litPars :: Term -> String -> Parser Term
 litPars term strLiteral = (\_ -> term) <$> string strLiteral <* whitespaces
 {-
     block
     infix
-    boolop
+    boolop D
     test
-    cmp
+    cmp D
     arith
     addop D
-    addend
+    addend T
     mulop D
-    factor
+    factor T
     atom T
-    cond
-    lambda
+    cond T
+    lambda T
     let
     equation T
     literal D
@@ -70,18 +89,25 @@ litTermParse = (\x -> Num x) <$> integer <|> boolPars
 keyWords :: [String]
 keyWords = ["if", "then", "else", "let", "in", "True", "False"]
 
+condPars :: Parser Term
+condPars = pure (\x y z -> Cond x y z) <* (string "if") <*> blockPars <* (string "then") <*> blockPars <* (string "else") <*> blockPars
+
 varPars :: Parser Term
 varPars = (\x -> Var x) <$> (identifier keyWords)
 
+lambdaPars :: Parser Term
+lambdaPars = pure (\x y -> Lambda x y) <* (parseOperator '\\' ) <*> (identifier keyWords) <* (string "->") <*> blockPars 
+
 eqnPars = (\var op block  _ -> Prim2 op var block) <$> varPars <*> parseOperator '=' <* whitespaces <*> blockPars <*> (char ';') <* whitespaces
 
-
+blockPars :: Parser Term
 blockPars = error "TODO"
 infixPars = error "TODO"
 
 parseOperator :: Char -> Parser String
 parseOperator c = ((\x -> [x]) <$> char c) <* whitespaces
 
+atomPars :: Parser Term
 atomPars = bracBlockPars <|> litTermParse <|> varPars
 
 bracBlockPars = parseOperator '(' *> blockPars <* parseOperator ')' 
